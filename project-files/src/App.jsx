b4e59bfe65734cfae9bcd3b0c3020e54c658fadb
Stefan Markov <stef4otm@gmail.com>
2024-10-29 16:15:27 +0200

    Finish commit

--- project-files/src/App.jsx
+++ project-files/src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: +newValue }; //+ -> прави стринга на число. Вместо Number().
+    });
+  }
+
   return (
     <>
       <Header />;
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && <p className="center">Duration must be greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
